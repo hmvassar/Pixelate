9d1eba36531ed158bd7e9afbd53b1be5093f6c20
Hunter Vassar <vassarhunter@gmail.com>
2023-04-27 21:59:33 -0400

    color selector

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // Your code here
+const selector = document.getElementById("color-select")
+
 
 const table = document.getElementsByTagName('table')[0]
 
@@ -13,3 +15,18 @@ table.appendChild(row)
  makeRow()
  makeRow()
  makeRow()
+
+const button = document.getElementById('add-row')
+ button.addEventListener('click', makeRow)
+
+table.addEventListener('click', colorize)
+
+function colorize (event) {
+    console.log(selector.value)
+    const target = event.target
+    if (target.className.length){
+        target.className = ''
+    } else{
+        target.className = selector.value
+    }
+}
